2d1ca6425ab897dbf18c8f939991ff88ad6011d3
yeziyue <huasifei@huasifei.com>
2026-07-03 14:52:55 +0800

    patches:修改fancontrol默认js文件，解决因为文件路径不对导致网页luci的风扇控制界面打不开的问题

--- patches/feeds/fancontrol/luci-app-fancontrol/htdocs/luci-static/resources/view/fancontrol.js
+++ patches/feeds/fancontrol/luci-app-fancontrol/htdocs/luci-static/resources/view/fancontrol.js
@@ -11,7 +11,7 @@ return view.extend({
 			uci.load('fancontrol')
 		]);
 	},
-	render: async function (data) {
+	render: function (data) {
 		var m, s, o;
 
 		m = new form.Map('fancontrol', _('Fan General Control'));
@@ -22,19 +22,31 @@ return view.extend({
 		o.rmempty = false;
 
 		o = s.option(form.Value, 'thermal_file', _('Thermal File'), _('Thermal File'));
-		o.placeholder = '/sys/devices/virtual/thermal/thermal_zone0/temp';
-		var temp_div = uci.get('fancontrol', 'settings', 'temp_div');
-		var temp = parseInt(await fs.read_direct(uci.get('fancontrol', 'settings', 'thermal_file')));
-		if (temp_div > 0 && temp > 0) {
-			o.description = _('Current temperature:') + ' <b>' + (temp / temp_div) + '°C</b>';
-		} else {
-			o.description = _('Thermal File');
-		}
+		o.placeholder = '/sys/class/thermal/thermal_zone0/temp';
+
+		// Safe read: don't crash if sysfs path doesn't exist
+		var thermal_path = uci.get('fancontrol', 'settings', 'thermal_file') || o.placeholder;
+		fs.read_direct(thermal_path).then(function (raw) {
+			var temp_div = uci.get('fancontrol', 'settings', 'temp_div') || 1000;
+			var temp = parseInt(raw);
+			if (temp_div > 0 && temp > 0) {
+				o.description = _('Current temperature:') + ' <b>' + (temp / temp_div) + '°C</b>';
+			}
+		}).catch(function () {
+			o.description = _('Thermal File (sensor not available)');
+		});
 
 		o = s.option(form.Value, 'fan_file', _('Fan File'), _('Fan Speed File'));
-		o.placeholder = '/sys/devices/virtual/thermal/cooling_device0/cur_state';
-		var speed = parseInt(await fs.read_direct(uci.get('fancontrol', 'settings', 'fan_file')));
-		o.description = _('Current speed:') + ' <b>' + (speed) + '</b>';
+		o.placeholder = '/sys/class/hwmon/hwmon1/pwm1';
+
+		// Safe read: don't crash if sysfs path doesn't exist
+		var fan_path = uci.get('fancontrol', 'settings', 'fan_file') || o.placeholder;
+		fs.read_direct(fan_path).then(function (raw) {
+			var speed = parseInt(raw);
+			o.description = _('Current speed:') + ' <b>' + speed + '</b>';
+		}).catch(function () {
+			o.description = _('Fan Speed File (device not available)');
+		});
 
 		o = s.option(form.Value, 'start_speed', _('Initial Speed'), _('Please enter the initial speed for fan startup.'));
 		o.placeholder = '35';
